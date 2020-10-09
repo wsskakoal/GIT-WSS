7f2dbc20f52f2adcec13fd7d72e2dbb5bba3329d
wylliank <wyllian@kinyx.com.br>
2020-10-09 17:08:50 -0400

    Add projetos

---
+++ TesteFireWall/TesteFireWall/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TesteFireWall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(358, 104);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "button1";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(215, 65);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(118, 20);
+            this.txtIp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(215, 107);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(118, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ TesteFireWall/TesteFireWall/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TesteFireWall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            OpenFirewallForProgram((Path.Combine(Application.StartupPath, "MySQL8\\bin\\mysqld.exe")), "Gestor_MySQL8");
+        }
+
+        private static void OpenFirewallForProgram(string exeFileName, string displayName)
+        {
+            var proc = Process.Start(
+                  new ProcessStartInfo
+                  {
+                      FileName = "netsh",
+                      Arguments =
+                              string.Format(
+                                  "advfirewall firewall add rule name=\"{0}\" dir=in action=allow program=\"{1}\" enable=yes",
+                                 displayName, exeFileName),
+                      WindowStyle = ProcessWindowStyle.Hidden,
+                      CreateNoWindow = true,
+                      RedirectStandardOutput = true,
+                      ErrorDialog = true,
+                      UseShellExecute = false,
+
+                  });
+            proc.WaitForExit();
+
+            var erro = proc.StandardOutput.ReadToEnd().ToString();
+
+            if (erro.Substring(0, 2) != "Ok")
+                MessageBox.Show($"Erro ao adicionar programa no firewall. {erro}");
+        }
+    }
+}
